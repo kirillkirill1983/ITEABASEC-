33732190bb2a5f5e4ddb80205738ae271bd7dcf5
baevkirill1983 <baevkirill1983@gmail.com>
2021-02-20 10:52:32 +0200

    no message

---
+++ Chapter_1/Home_work_1/Home_work_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите метры: ");
+            int metrTotal = Convert.ToInt32(Console.ReadLine());
+            //int meterTotal = centimeterTotal + 1000;
+            Console.WriteLine($"Метры {metrTotal} это будет {metrTotal *1000} сантиметров");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter_1/Home_work_1/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+            Console.WriteLine("Введите радиус");
+            int radiusStart = Convert.ToInt32(Console.ReadLine());
+            var squareTotal = Math.Pow((PI * radiusStart),2);
+            Console.WriteLine("Площадь круга {0}",squareTotal);
+            Console.ReadKey();
+        }
+    }
+}
