57126e97f400c36d9dd963725f25b8db1540e147
baevkirill1983 <baevkirill1983@gmail.com>
2021-02-20 10:57:28 +0200

    Добавление

---
+++ Chapter_1/Home_work_1/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение первого катета");
+            var legOne= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение второго катета");
+            var legTwo = Convert.ToInt32(Console.ReadLine());
+            var hypotenuseTotal = Math.Sqrt((Math.Pow(legOne, 2)+Math.Pow(legTwo,2)));
+            Console.WriteLine("Гипотенуза {0}", hypotenuseTotal);
+            Console.ReadKey();
+        }
+    }
+}
