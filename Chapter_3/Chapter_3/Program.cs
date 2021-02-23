f4d538f103c4577e0403606babbc29ed2a5a8dcc
baevkirill1983 <baevkirill1983@gmail.com>
2021-02-23 15:44:17 +0200

    Добавлено 1.3

---
+++ Chapter_3/Chapter_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i <= 5)
+            {
+                Console.WriteLine($"Значение {i}");
+                i++;
+               
+            }
+            ////////////////
+            do
+            {
+                Console.WriteLine($"Значение {i}");
+                i++;
+            } while (i<5);
+            ///////////////
+            for (int y= 0; y < 5; y++)
+            {
+                Console.WriteLine($"Значение {y}");
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter_3/Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine($"Значение {i} +3 => {i*3}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
