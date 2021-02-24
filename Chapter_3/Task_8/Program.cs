14cba7aa9e61f02a146d3e6f4dc98895ac700700
baevkirill1983 <baevkirill1983@gmail.com>
2021-02-24 15:52:01 +0200

    no message

--- Chapter_3/Task_8/Program.cs
+++ Chapter_3/Task_8/Program.cs
@@ -10,10 +10,21 @@ namespace Task_8
     {
         static void Main(string[] args)
         {
-            Console.Write("Ведите число фибоначи : ");
-            var fobonachiNamber = Convert.ToInt32(Console.Read());
+            Console.Write("Ведите число фибоначи=> : ");
+            int fibonachiNamber = Convert.ToInt32(Console.ReadLine());
 
+            int f1 = 0;
+            int f2 = 1;
+            for (int i = 0; i < fibonachiNamber; i++)
+            {
+                int fib = f1 + f2;
+                Console.WriteLine(fib);
+                f1 = f2;
+                f2 = fib;
+            }
             Console.ReadKey();
+
         }
     }
+
 }
