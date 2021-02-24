f1d9541244ff3ebfd8cd64decc85d0cb2a2de760
baevkirill1983 <baevkirill1983@gmail.com>
2021-02-24 12:26:30 +0200

    no message

---
+++ Chapter_3/Task_8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ведите число фибоначи : ");
+            var fobonachiNamber = Convert.ToInt32(Console.Read());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter_3/Task_9/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число ");
+            var oneNamber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число ");
+            var twoNamber = Convert.ToInt32(Console.ReadLine());
+            
+            
+
+            if (oneNamber>twoNamber)
+            {
+                int sum=0;
+                Console.WriteLine("Значение 1>2");
+                
+                for (int i = twoNamber; i < oneNamber; i++)
+                {
+                    sum += i;
+
+                }
+                Console.WriteLine("Сумма промежутка равна {0}",sum);
+            }
+
+            if (oneNamber < twoNamber)
+            {
+                
+                Console.WriteLine("Значение 1<2");
+
+                for (int i = oneNamber; i < twoNamber; i++)
+                {
+                    if (i%2!=0)
+                    {
+                        Console.WriteLine($"Число {i} ");
+                    }
+                }
+             }
+
+            Console.ReadKey();
+        }
+    }
+}
