6ca9a312032950252e75a0c9f559c0279d081021
baevkirill1983 <baevkirill1983@gmail.com>
2021-03-03 19:47:00 +0200

    no message

---
+++ Chapter_4/Task_11/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сумма вклада");
+            var totalCash = Convert.ToInt32(Console.ReadLine());
+
+            while (true)
+            {
+                Console.WriteLine("Введите сумму платежа");
+                var amounеOfPayment = Convert.ToInt32(Console.ReadLine());
+                
+                LoanBalance(ref totalCash, amounеOfPayment);
+
+                CashInfo(totalCash);
+                if (totalCash <= 0)
+                {
+                    Console.WriteLine("Кредит выплачен");
+                    break;
+                }
+            }
+            Console.ReadKey();
+
+        }
+
+        static void CashInfo(int var) 
+        {
+            
+            if (var<0)
+            {
+                Console.WriteLine($"Кредит составляет => {var}");
+                
+            }
+            if (var>=0)
+            {
+                Console.WriteLine($"Кредит составляет => {var}");
+            }
+            
+        }
+
+        static void LoanBalance(ref int variable1, int variable2)
+        {
+            variable1 -= variable2;
+
+            if (variable1 <= 0)
+            {
+                int temp = variable1;
+                Console.WriteLine("Переплата по плетежу {0}", Math.Abs(temp));
+            }
+
+            //if (variable1 < variable2)
+            //{
+            //    Console.WriteLine("остаток платежа {0}", variable1);
+            //}
+         }
+    }
+}
