c66ef5612e356a6095d8b7e2503a3d997b979fe2
baevkirill1983 <baevkirill1983@gmail.com>
2021-03-10 12:05:09 +0200

    Добавление Массивы

---
+++ Chapter_5/Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Enter Item=>");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"array[{i}] => {array[i]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
