7a1529544ca1ca272b99c9c0eda8950e680ffd46
baevkirill1983 <baevkirill1983@gmail.com>
2021-03-10 12:25:17 +0200

    no message

--- Chapter_5/Task_2/Program.cs
+++ Chapter_5/Task_2/Program.cs
@@ -10,6 +10,20 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            int[] array = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Enter Item=>");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = array.Length-1; i>=0; --i)
+            {
+                Console.WriteLine($"array[{i}]=> {array[i]} ");
+            }
+
+            Console.ReadKey();
+            
         }
     }
 }
