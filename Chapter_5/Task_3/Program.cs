a073a1fe09a5b1c6ab3069b2e02c82251f7b661b
baevkirill1983 <baevkirill1983@gmail.com>
2021-03-10 13:02:46 +0200

    no message

---
+++ Chapter_5/Task_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arrayA =new char[5];
+            char[] arrayB = new char[5];
+            char[] arrayTemp = new char[5];
+            for (int i = 0; i < arrayA.Length; i++)
+            {
+                Console.WriteLine("Enter Char=>");
+                arrayA[i] = Convert.ToChar(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arrayA.Length; i++)
+            {
+                arrayB[i] = arrayA[(arrayA.Length-1) - i];
+               
+            }
+            for (int i = 0; i < arrayB.Length; i++)
+            {
+                Console.WriteLine($"array[{i}] => {arrayB[i]}");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
