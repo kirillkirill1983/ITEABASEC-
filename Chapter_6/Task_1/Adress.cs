1a3809088657066ac8ab538bb1b836f8d32e67a2
baevkirill1983 <baevkirill1983@gmail.com>
2021-03-12 16:25:25 +0200

    no message

---
+++ Chapter_6/Task_1/Adress.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Adress
+    {
+        private string country = string.Empty;
+        private string city = string.Empty;
+        private string street = string.Empty;
+        private int house;
+        private int apartment;
+        private int index;
+
+        public string Country
+        {
+            set  
+            {
+                country= value;
+            }
+            
+            get  
+            {
+                return country;
+            }
+        }
+        public string City
+        {
+            set
+            {
+                city = value;
+            }
+
+            get
+            {
+                return city;
+            }
+        }
+        public string Street
+        {
+            set
+            {
+                street =value;
+            }
+
+            get
+            {
+                return street;
+            }
+        }
+        public int House
+        {
+            set
+            {
+                house = value;
+            }
+
+            get
+            {
+                return house;
+            }
+        }
+        public int Apartment
+        {
+            set
+            {
+                apartment = value;
+            }
+
+            get
+            {
+                return apartment;
+            }
+        }
+
+        public int Index
+        {
+            set
+            {
+                index = value;
+            }
+
+            get
+            {
+                return index;
+            }
+        }
+        public void ShowAdress() 
+        {
+            Console.WriteLine($"{country} ,{city}, {street}," +
+                $"{house}, {apartment}, {index} ");
+        }
+
+    }
+}
---
+++ Chapter_6/Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adress adress = new Adress();
+            adress.House = 12;
+            adress.Country = "Vavilon";
+            adress.Index = 123;
+            adress.City = "Borodok";
+            adress.Apartment = 100;
+            adress.Street = "beer_lager";
+            adress.ShowAdress();
+            Console.ReadKey();
+        }
+    }
+}
