55112512ecd69c0f13331f407588dc6d24e0bd79
baevkirill1983 <baevkirill1983@gmail.com>
2021-03-23 19:53:58 +0200

    no message

---
+++ Chapter_8/Task_2/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class Player : IPlayable, IRecodable
+    {
+        void IPlayable.Pause() 
+        {
+            Console.WriteLine("Pause");
+        }
+        void IPlayable.Play() 
+        {
+            Console.WriteLine("Play");
+        }
+        void IPlayable.Stop() 
+        {
+            Console.WriteLine("Stop");
+        }
+
+        void IRecodable.Pause() 
+        {
+            Console.WriteLine("Pause");
+        }
+        void IRecodable.Record() 
+        {
+            Console.WriteLine("Record");
+        }
+        void IRecodable.Stop() 
+        {
+            Console.WriteLine("Stop");
+        }
+    }
+}
---
+++ Chapter_8/Task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            Console.WriteLine("Play or Recodr");
+            string movePlay = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(new string('=', 10));
+            switch (movePlay)
+            {
+                case "Play":
+                    IPlayable playable = player;// можно дальше развить линию выбора ченго именно хочет душа 
+                    playable.Play();
+                    playable.Pause();
+                    playable.Stop();
+                    break;
+                case "Record":
+                    IRecodable recodable = player;
+                    recodable.Pause();
+                    recodable.Stop();
+                    recodable.Record();
+                    break;
+                default:
+                    Console.WriteLine("Ошибка ввода");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
