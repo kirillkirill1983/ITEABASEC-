287829c0f888b16e8bbeab7a764cf57a89b514f1
baevkirill1983 <baevkirill1983@gmail.com>
2021-03-24 09:00:00 +0200

    no message

---
+++ Chapter_8/Task_4/DOCHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    public class DOCHandler:AbstractHandler
+    {
+        public override void Change()
+        {
+            Console.WriteLine("DOC Change");
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine("DOC Creat");
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("DOC Open");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("DOC Save");
+        }
+
+    }
+}
--- Chapter_8/Task_4/Program.cs
+++ Chapter_8/Task_4/Program.cs
@@ -12,14 +12,30 @@ namespace Task_4
         {
             TXTHandler tXTHandler = new TXTHandler();
             XMLHabdler xMLHabdler = new XMLHabdler();
+            DOCHandler dOCHandler = new DOCHandler();
+            Console.WriteLine("Введите тип документа txt xml doc");
+
             string docoment = Convert.ToString(Console.ReadLine()); 
+            
             switch (docoment)
             {
                 case "txt":
                     tXTHandler.Change();
+                    tXTHandler.Save();
+                    tXTHandler.Open();
+                    tXTHandler.Create();
                     break;
                 case "xml":
                     xMLHabdler.Change();
+                    xMLHabdler.Save();
+                    xMLHabdler.Open();
+                    xMLHabdler.Create();
+                    break;
+                case "doc":
+                    dOCHandler.Change();
+                    dOCHandler.Save();
+                    dOCHandler.Open();
+                    dOCHandler.Create();
                     break;
                 default:
                     break;
