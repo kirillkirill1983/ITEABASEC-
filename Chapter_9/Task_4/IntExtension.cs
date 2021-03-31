31511eda0000e7e646e56cfe38b46720e954152b
baevkirill1983 <baevkirill1983@gmail.com>
2021-03-31 11:30:42 +0300

    no message

---
+++ Chapter_9/Task_4/IntExtension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace Task_4
+{
+    public static class IntExtension
+    {
+        public static int[] IntSort(this int[] temp) 
+        {
+           
+            int count = 0;
+           
+            for (int i = 0; i < temp.Length - 1; i++)
+            {
+                for (int j = i + 1; j < temp.Length; j++)
+                {
+                    if (temp[i] > temp[j])
+                    {
+                        count = temp[i];
+                        temp[i] = temp[j];
+                        temp[j] = count;
+                    }
+                }
+            }
+            return temp;
+        }
+
+        public static int[] IntRandom(int value) 
+        {
+            Random random = new Random();
+            int[] temp = new int[value];
+            
+            for (int i = 0; i < value; i++)
+            {
+                temp[i]= random.Next(10);
+            }
+            return temp;
+        }
+    }
+
+}
---
+++ Chapter_9/Task_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraySort = IntExtension.IntRandom(5);
+            for (int i = 0; i < arraySort.Length; i++)
+            {
+                Console.Write($" {arraySort[i] }");
+            }
+
+            Console.WriteLine("\n {0}",new string('-', 10));
+            arraySort.IntSort();
+            for (int i = 0; i < arraySort.Length; i++)
+            {
+                Console.Write($" {arraySort[i]} ");
+            }
+
+            Console.ReadKey();
+        }
+        
+    }
+}
