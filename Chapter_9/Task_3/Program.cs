73068570a0928fd26e35797c40f7aded0859e164
baevkirill1983 <baevkirill1983@gmail.com>
2021-03-31 11:06:06 +0300

    no message

--- Chapter_9/Task_3/Program.cs
+++ Chapter_9/Task_3/Program.cs
@@ -13,7 +13,9 @@ namespace Task_3
             string Str = "Выходные= пиво + сон";
             int result= StringExtension.StrCount(Str,2);
             Console.WriteLine($"{result }");
+
             Console.WriteLine(new string('-', 10));
+
             Console.WriteLine(Str.StrCount(2).ToString());
 
             Console.ReadKey();
