df8bbc8e8044bf02153c1a1c2f1794a87c274cff
baevkirill1983 <baevkirill1983@gmail.com>
2021-03-31 13:37:28 +0300

    no message

--- Chapter_9/Task_5/PointXYZ.cs
+++ Chapter_9/Task_5/PointXYZ.cs
@@ -35,12 +35,13 @@ namespace Task_5
 
         public static PointXYZ operator /(PointXYZ point1, PointXYZ point2)
         {
-            return new PointXYZ(point1.X / point2.X, point1.Y / point2.Y, point1.Z / point2.Z);
+            
+            return new PointXYZ(point1.X/point2.Y , point1.Y / point2.Y, point1.Z / point2.Z);
         }
 
         public override string ToString()
         {
-            return String.Format($"{this.X } : {this.Y} : {this.Z} "); 
+            return String.Format($"{this.X} : {this.Y} : {this.Z} "); 
         }
 
         public override bool Equals(object o)
