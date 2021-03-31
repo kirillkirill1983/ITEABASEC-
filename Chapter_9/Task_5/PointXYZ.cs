b1672e055d98a41dc0952219a120433029224732
baevkirill1983 <baevkirill1983@gmail.com>
2021-03-31 13:26:13 +0300

    no message

---
+++ Chapter_9/Task_5/PointXYZ.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    public class PointXYZ
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public PointXYZ(int pX,int pY,int pZ )
+        {
+            X = pX;
+            Y = pY;
+            Z = pZ;
+        }
+
+        public static PointXYZ operator + (PointXYZ point1,PointXYZ point2) 
+        {
+            return new PointXYZ(point1.X + point2.X,point1.Y+point2.Y,point1.Z+point2.Z);   
+        }
+
+        public static PointXYZ operator -(PointXYZ point1, PointXYZ point2)
+        {
+            return new PointXYZ(point1.X - point2.X, point1.Y - point2.Y, point1.Z - point2.Z);
+        }
+
+        public static PointXYZ operator *(PointXYZ point1, PointXYZ point2)
+        {
+            return new PointXYZ(point1.X * point2.X, point1.Y * point2.Y, point1.Z * point2.Z);
+        }
+
+        public static PointXYZ operator /(PointXYZ point1, PointXYZ point2)
+        {
+            return new PointXYZ(point1.X / point2.X, point1.Y / point2.Y, point1.Z / point2.Z);
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{this.X } : {this.Y} : {this.Z} "); 
+        }
+
+        public override bool Equals(object o)
+        {
+            if (o is PointXYZ)
+            {
+                if (((PointXYZ)o).X == this.X && ((PointXYZ)o).Y == this.Y && ((PointXYZ)o).Z == this.Z  )
+                    return true;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+    }
+}
---
+++ Chapter_9/Task_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PointXYZ pointXYZ1 = new PointXYZ(2, 4, 7);
+            PointXYZ pointXYZ2 = new PointXYZ(4, 6, 7);
+            
+
+            Console.WriteLine($"{ pointXYZ1} +{pointXYZ2} ={pointXYZ1+pointXYZ2}"+"\n");
+            Console.WriteLine($"{ pointXYZ1} -{pointXYZ2} ={pointXYZ1 - pointXYZ2}"+"\n");
+            Console.WriteLine($"{ pointXYZ1} *{pointXYZ2} ={pointXYZ1 * pointXYZ2}"+"\n");
+            Console.WriteLine($"{pointXYZ1} /{pointXYZ2} ={pointXYZ1 / pointXYZ2}"+"\n");
+            Console.WriteLine($"{pointXYZ1.Equals(pointXYZ2)}");
+            Console.WriteLine($"HASH {pointXYZ1} = {pointXYZ1.GetHashCode()}");
+            Console.WriteLine($"HASH {pointXYZ2} = {pointXYZ2.GetHashCode()}");
+            Console.ReadKey();
+        }
+    }
+}
